3ee9a1efb09929513bdbbb5c1d5c6ab59d149728
Sally <38636017+potatofarmersally@users.noreply.github.com>
2019-01-04 20:58:30 -0800

    Read in files, and built in text files

--- app/src/main/java/proj/sallys/handmaidgenerator/TheMagicHappens.java
+++ app/src/main/java/proj/sallys/handmaidgenerator/TheMagicHappens.java
@@ -4,9 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.util.ArrayList;
+
 public class TheMagicHappens extends AppCompatActivity {
 
     private EditText name;
+    private ArrayList<String> boyList;
+    private ArrayList<String> girlList;
 
     @Override //basically the constructor
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +23,24 @@ public class TheMagicHappens extends AppCompatActivity {
         setContentView(R.layout.activity_the_magic_happens);
 
         name = findViewById(R.id.nameBox);
+        boyList = new ArrayList<String>();
+        girlList = new ArrayList<String>();
     }
 
+    public void parse() {
+
+        try {
+            BufferedReader br =
+                    new BufferedReader(new FileReader("app/sampledata/boyNames.txt"));
+            BufferedReader brGirl =
+                    new BufferedReader(new FileReader("app/sampledata/girlNames.txt"));
+
+        }
+        catch (FileNotFoundException e){
+
+        }
+
+    }
 
 
 }
